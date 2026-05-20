6b74a873c17b2f058c82e7c060e250e974807844
Michael Bino <bino@princeton.edu>
2026-05-20 13:23:06 -0400

    Mirror of https://fan60.princeton.edu on Wed May 20 13:23:06 EDT 2026

--- core/assets/vendor/jquery.ui/ui/data-min.js
+++ core/assets/vendor/jquery.ui/ui/data-min.js
@@ -7,4 +7,3 @@
  * https://jquery.org/license
  */
 !function(e){"use strict";"function"==typeof define&&define.amd?define(["jquery","./version"],e):e(jQuery)}((function(e){"use strict";return e.extend(e.expr.pseudos,{data:e.expr.createPseudo((function(n){return function(t){return!!e.data(t,n)}}))})}));
-//# sourceMappingURL=data-min.js.map
--- core/assets/vendor/jquery.ui/ui/disable-selection-min.js
+++ core/assets/vendor/jquery.ui/ui/disable-selection-min.js
@@ -7,4 +7,3 @@
  * https://jquery.org/license
  */
 !function(e){"use strict";"function"==typeof define&&define.amd?define(["jquery","./version"],e):e(jQuery)}((function(e){"use strict";return e.fn.extend({disableSelection:(n="onselectstart"in document.createElement("div")?"selectstart":"mousedown",function(){return this.on(n+".ui-disableSelection",(function(e){e.preventDefault()}))}),enableSelection:function(){return this.off(".ui-disableSelection")}});var n}));
-//# sourceMappingURL=disable-selection-min.js.map
